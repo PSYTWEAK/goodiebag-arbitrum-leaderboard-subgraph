9dd08cf2f7ba5ae7077bbc01aae09240ef41117e
PSYTWEAK <psy@promise.exchange>
2022-08-17 16:16:22 +0100

    remove count

--- src/goodie-bag.ts
+++ src/goodie-bag.ts
@@ -1,45 +1,17 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { GoodieBag, MultiBuy } from "../generated/GoodieBag/GoodieBag"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { GoodieBag, MultiBuy } from "../generated/GoodieBag/GoodieBag";
+import { User } from "../generated/schema";
 
 export function handleMultiBuy(event: MultiBuy): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = User.load(event.transaction.from.toHex());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new User(event.transaction.from.toHex());
+    entity.account = event.params.account;
+    entity.value = event.params.value;
+  } else {
+    entity.value = entity.value.plus(event.params.value);
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-  entity.value = event.params.value
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.weth(...)
+  entity.save();
 }
